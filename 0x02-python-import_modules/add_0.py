6b10ec0c26bbc4f3fd29c2e2d16f4e60955d0940
JamesOdera <nyangoma.odera@gmail.com>
2022-08-25 00:53:09 -0700

    commit changes

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: integer 1
+        b: int 2
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
