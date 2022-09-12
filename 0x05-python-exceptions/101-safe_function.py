fb81a62c253b3459570a40acdce30026956139d0
JamesOdera <nyangoma.odera@gmail.com>
2022-09-12 02:42:53 -0700

    commit changes

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,8 +3,8 @@
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return (result)
-    except:
+        return result
+    except Exception as error:
         import sys
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return None
