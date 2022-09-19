2998c6db7a8c9c65e7bc57ff468108f681832384
JamesOdera <nyangoma.odera@gmail.com>
2022-09-19 03:24:30 -0700

    commit file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -31,7 +31,8 @@ class Rectangle:
     def width(self, width):
         '''method: set_width setter
         '''
-        if not isinstance(self.__width, int) or isinstance(self.__width, bool):
+        if not isinstance(self.__width, int) or isinstance(self.__width,
+                bool):
             raise TypeError("width must be an integer")
         if self.__width < 0:
             raise ValueError("width must be >= 0")
