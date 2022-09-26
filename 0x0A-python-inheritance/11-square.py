8e0f807189692acf261ef20a9eb8076025a6eb39
JamesOdera <nyangoma.odera@gmail.com>
2022-09-26 07:12:35 -0700

    commit file

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,7 +16,7 @@ class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
         """ size init"""
-        self.__size = 
+        self.__size = size
         super().__init__(self.__size, self.__size)
 
     def __str__(self):
