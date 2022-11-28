3b7d6663fa1aa08bc9c9dfba356b98752dbe8125
JamesOdera <nyangoma.odera@gmail.com>
2022-11-28 05:17:09 -0800

    commit file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 if (process.argv.length === 0) {
 	  console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (process.argv.length === 1) {
 	  console.log('Argument found');
 } else {
 	  console.log('Arguments found');
