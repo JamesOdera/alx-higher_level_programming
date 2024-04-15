e594e698150846f9e1e636a9439629092ff78326
JamesOdera <nyangoma.odera@gmail.com>
2024-04-15 06:49:13 +0000

    commit file

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-'''
-    Defines classes for tables
-'''
-from model_state import Base
+"""
+Class definition of a City
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from model_state import Base
 
 
 class City(Base):
-    '''
-        Creates table for cities
-    '''
+    """
+    City class that inherits from Base
+
+    Attributes:
+        id: Id city
+        name: Name of the city
+        state_id: State id
+    """
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True,
-            primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-class definition of a State and an instance Base = declarative_base()
-"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, text, ForeignKey
 from relationship_state import Base
+"""
+    Module that performs creates a States class based off of Base.
+"""
 
 
 class City(Base):
     """
-    City class:
-    inherits from Base
+        The ``City`` class which inherits from ``Base`` class.
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
